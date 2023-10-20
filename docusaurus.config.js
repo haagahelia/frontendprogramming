663476593dec8e7a849bf4ab618e462d253637cb
juhahinkula <hinkula@gmail.com>
2023-10-20 15:49:58 +0300

    docusaurus upgrade

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer').themes.github;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -40,7 +40,6 @@ const config = {
       ({
         docs: {
           remarkPlugins: [
-            [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
           ],
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
@@ -49,14 +48,9 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         pages: {
-          remarkPlugins: [require('@docusaurus/remark-plugin-npm2yarn')],
         },
         blog: {
           remarkPlugins: [
-            [
-              require('@docusaurus/remark-plugin-npm2yarn'),
-              {converters: ['pnpm']},
-            ],
           ],
           showReadingTime: true,
           // Please change this to your repo.
