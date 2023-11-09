76104afba927ded35aca5fe74f6d2ab0e6881ec1
juhahinkula <hinkula@gmail.com>
2023-11-09 12:34:07 +0200

    config changed to haagahelia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,16 +10,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://haagahelia.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/frontendprogramming/',
   plugins: ['@docusaurus/theme-live-codeblock'],
  
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'haagahelia', // Usually your GitHub org/user name.
-  projectName: 'frontend', // Usually your repo name.
+  projectName: 'frontendprogramming', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
